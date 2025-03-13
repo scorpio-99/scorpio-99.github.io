63a7c7210458a773b0b9d03e57bbb15e02ee583e
Scorpio <luca-meli@outlook.com>
2025-03-13 17:02:08 +0100

    refactoring style

--- js/bucket-list.js
+++ js/bucket-list.js
@@ -1,7 +1,7 @@
 function displayBucketList() {
     const container = document.getElementById('plans-list');
     container.innerHTML = data.bucketList.map(plan => `
-        <li class="plan-item">
+        <li class="plan-item card hover-grow">
             <div class="plan-text">${plan.item}</div>
             <div class="plan-status">${plan.status}</div>
         </li>
--- js/fun-facts.js
+++ js/fun-facts.js
@@ -1,7 +1,7 @@
 function displayFunFacts() {
     const container = document.getElementById('facts-container');
     container.innerHTML = data.funFacts.map(fact => `
-        <div class="fact">
+        <div class="fact card hover-grow">
             <div class="number">${fact.number}</div>
             <div class="label">${fact.label}</div>
         </div>

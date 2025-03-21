6cabcfcd4da4f25c9cb60198d20e46dae6ef6bde
Scorpio <luca-meli@outlook.com>
2025-03-21 16:23:21 +0100

    adjust colors, use custom map icon marker

--- js/places.js
+++ js/places.js
@@ -25,7 +25,7 @@ function initMap() {
     
     // Custom marker icon
     const customIcon = L.icon({
-        iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+        iconUrl: 'assets/marker-icon.png',
         shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
         iconSize: [25, 41],
         iconAnchor: [12, 41],

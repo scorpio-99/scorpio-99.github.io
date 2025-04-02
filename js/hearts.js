e630f3f1b2ade3f7995814e673fc424b118be143
Scorpio <luca-meli@outlook.com>
2025-04-02 16:31:54 +0200

    remove old html, js and css build

--- js/hearts.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('click', function(e) {
-    const heart = utils.createElement('floating-heart', '❤️');
-    heart.style.left = e.pageX + 'px';
-    heart.style.top = e.pageY + 'px';
-    document.body.appendChild(heart);
-    
-    setTimeout(() => heart.remove(), ANIMATION_SETTINGS.heartDuration);
-}); 
--- js/quotes.js
+++
@@ -1,8 +0,0 @@
-function updateQuote() {
-    const quoteElement = document.getElementById('quote');
-    const randomQuote = data.quotes[Math.floor(Math.random() * data.quotes.length)];
-    quoteElement.textContent = randomQuote;
-}
-
-setInterval(updateQuote, QUOTE_INTERVAL);
-updateQuote(); 

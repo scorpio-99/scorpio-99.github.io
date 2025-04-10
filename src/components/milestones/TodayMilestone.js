4b36a13e9069b23d59f68b9139f4967e41c3271e
Scorpio <luca-meli@outlook.com>
2025-04-10 16:23:53 +0200

    fix date format of milestone today

--- src/components/milestones/TodayMilestone.js
+++ src/components/milestones/TodayMilestone.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Section from '../common/Section';
 import {format} from 'date-fns';
+import {DATE_FORMAT} from '../../data/constants'
 
 const TodayMilestone = ({milestone}) => {
     return (
@@ -9,7 +10,7 @@ const TodayMilestone = ({milestone}) => {
                 <div className="milestone-label">🎉 Today's Milestone! 🎉</div>
                 <div className="milestone-content">
                     <div className="milestone-event">{milestone.milestone}</div>
-                    <div className="milestone-date">{format(milestone.date, 'MMMM d, yyyy')}</div>
+                    <div className="milestone-date">{format(milestone.date, DATE_FORMAT)}</div>
                     <div className="milestone-days">Today is the day!</div>
                 </div>
             </div>
